ff0946a59a30836052299ef34e91d4338882a6d2
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-07-31 16:16:11 +0530

    Add palindrome checking functionality in Java

---
+++ assignments/4.functions/10.palindrome of a number.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static boolean isPalindrome(int n) {
+        int original = n;
+        int reverse = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            reverse = reverse * 10 + digit;
+            n = n / 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        if (isPalindrome(n))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
+    }
+}
