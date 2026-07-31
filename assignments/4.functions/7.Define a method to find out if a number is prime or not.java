5da76667857ab895cccf9d1ca9e1cb9c0d23f3aa
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-07-31 16:13:19 +0530

    Add method to check if a number is prime

---
+++ assignments/4.functions/7.Define a method to find out if a number is prime or not.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+    static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        if (isPrime(n))
+            System.out.println("Prime");
+        else
+            System.out.println("Not Prime");
+    }
+}
