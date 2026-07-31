ab544f142838fd0bb6f022f092417257ccdda132
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-07-31 16:07:02 +0530

    Add Java program to check vote eligibility

---
+++ assignments/4.functions/3.check vote eligibility.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Main {
+    static void checkEligibility(int age) {
+        if (age >= 18)
+            System.out.println("Eligible to vote");
+        else
+            System.out.println("Not eligible to vote");
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        checkEligibility(age);
+    }
+}
