0dfa509c1925ab74dc29017633bb6f4db6f14ee7
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-07-31 16:08:15 +0530

    Add program to calculate sum of two user inputs

---
+++ assignments/4.functions/4.Write a program to print the sum of two numbers entered by user by defining your own method.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    static void sum(int a, int b) {
+        System.out.println("Sum = " + (a + b));
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter two numbers: ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        sum(a, b);
+    }
+}
