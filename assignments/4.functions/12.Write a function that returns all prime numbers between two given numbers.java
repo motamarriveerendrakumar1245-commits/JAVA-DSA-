3f9ff05c5e863d24d20e67f943260a14ce31cb2f
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-07-31 16:19:42 +0530

    Create 12.Write a function that returns all prime numbers between two given numbers.java

---
+++ assignments/4.functions/12.Write a function that returns all prime numbers between two given numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Main {
+    static boolean isPrime(int n) {
+        if (n < 2)
+            return false;
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0)
+                return false;
+        }
+        return true;
+    }
+    static void printPrimes(int start, int end) {
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i))
+                System.out.print(i + " ");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter start and end: ");
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        printPrimes(start, end);
+    }
+}
