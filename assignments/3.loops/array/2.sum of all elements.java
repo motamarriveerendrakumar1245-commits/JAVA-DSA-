c0566c88b053f27f8a273692d3d125b48145b405
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:46:56 +0530

    Create 2.sum of all elements.java

---
+++ assignments/3.loops/array/2.sum of all elements.java
@@ -0,0 +1,8 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int sum = 0;
+        for (int x : arr) sum += x;
+        System.out.println("Sum = " + sum);
+    }
+}
