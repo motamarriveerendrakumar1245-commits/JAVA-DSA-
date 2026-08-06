dcbe26463b7fc03bfc6bb523fec201da35a3a7ea
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:55:18 +0530

    Add RotateLeft class to rotate array left by 1

---
+++ assignments/3.loops/array/15.rotate array left by 1.java
@@ -0,0 +1,9 @@
+public class RotateLeft {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int temp = arr[0];
+        for (int i = 0; i < arr.length - 1; i++) arr[i] = arr[i+1];
+        arr[arr.length - 1] = temp;
+        for (int x : arr) System.out.print(x + " ");
+    }
+}
