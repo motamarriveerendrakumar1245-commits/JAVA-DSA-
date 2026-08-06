d9f07e8af7c3663cd8523da85de91d802599ead0
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:49:12 +0530

    Create 6.count even or odd numbers.java

---
+++ assignments/3.loops/array/6.count even or odd numbers.java
@@ -0,0 +1,10 @@
+public class CountEvenOdd {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int even = 0, odd = 0;
+        for (int x : arr) {
+            if (x % 2 == 0) even++; else odd++;
+        }
+        System.out.println("Even = " + even + ", Odd = " + odd);
+    }
+}
