e19005913935958ee1ae5bcf669e84a487bcc60a
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:55:53 +0530

    Add program to rotate array right by 1

---
+++ assignments/3.loops/array/16.rotate array right by 1.java
@@ -0,0 +1,9 @@
+public class RotateRight {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int temp = arr[arr.length - 1];
+        for (int i = arr.length - 1; i > 0; i--) arr[i] = arr[i-1];
+        arr[0] = temp;
+        for (int x : arr) System.out.print(x + " ");
+    }
+}
