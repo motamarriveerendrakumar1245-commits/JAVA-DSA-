4f344c856f85ee51c1c8e1affed1ba60a42bd536
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:43:54 +0530

    Add MainDiagonal class to print main diagonal elements

---
+++ assignments/3.loops/matrix/6.main diagonal.java
@@ -0,0 +1,6 @@
+public class MainDiagonal {
+    public static void main(String[] args) {
+        int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        for (int i = 0; i < mat.length; i++) System.out.print(mat[i][i] + " ");
+    }
+}
