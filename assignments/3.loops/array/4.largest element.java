300bf555fe2c3ac9565f20011ea09f694b2d472c
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:48:05 +0530

    Create 4.largest element.java

---
+++ assignments/3.loops/array/4.largest element.java
@@ -0,0 +1,8 @@
+public class LargestElement {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        int max = arr[0];
+        for (int x : arr) if (x > max) max = x;
+        System.out.println("Largest = " + max);
+    }
+}
