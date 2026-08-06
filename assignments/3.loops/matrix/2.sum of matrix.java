748ed8fcb7857ab6084674758f17d3aaa86e4e59
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-06 14:41:04 +0530

    Create 2.sum of matrix.java

---
+++ assignments/3.loops/matrix/2.sum of matrix.java
@@ -0,0 +1,7 @@
+public class SumMatrix {
+    public static void main(String[] args) {
+        int[][] mat = {{1,2},{3,4}};
+        int sum = 0; for (int[] row : mat) for (int x : row) sum += x;
+        System.out.println(sum);
+    }
+}
