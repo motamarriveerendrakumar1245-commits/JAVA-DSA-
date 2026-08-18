3f5d2b3579025e7e50a2d75b64a8fd72aa4ef3b5
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-18 14:35:34 +0530

    Create 4.reverse list.java

---
+++ assignments/linked list/4.reverse list.java
@@ -0,0 +1,48 @@
+class ReverseList {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        return prev;   // new head
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+
+        System.out.print("Original List: ");
+        printList(head);
+
+        head = reverse(head);
+
+        System.out.print("Reversed List: ");
+        printList(head);
+    }
+}
