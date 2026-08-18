772d03c4db06c01a40fdb65fc310d4a741637d26
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-18 14:30:41 +0530

    Implement linked list node counting functionality

---
+++ assignments/linked list/2.count node.java
@@ -0,0 +1,42 @@
+class CountNodes {
+    static class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    static int countNodes(Node head) {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+
+        System.out.print("Linked List: ");
+        printList(head);
+
+        System.out.println("Total number of nodes = " + countNodes(head));
+    }
+}
