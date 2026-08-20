4b8321ea575d643a1a01047378cc9692a014c9dd
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-20 15:06:17 +0530

    Add Person class with constructor and display method

---
+++ assignments/constructor/constructor.java
@@ -0,0 +1,20 @@
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void display() {
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
+
+public class ConstructorExample {
+    public static void main(String[] args) {
+        Person p = new Person("Anita", 20);
+        p.display();
+    }
+}
