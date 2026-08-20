120f4f2b02c20236e0fe635448018b61a77d5810
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-20 15:01:08 +0530

    Create inert.java

---
+++ assignments/linked list/double linked ist/inert.java
@@ -0,0 +1,41 @@
+class Node {
+    int data;
+    Node next, prev;
+
+    Node(int newData) {
+        data = newData;
+        next = prev = null;
+    }
+}
+
+public class DoublyLinkedListInsert {
+    public static Node insert(Node head, int newData) {
+        Node newNode = new Node(newData);
+        newNode.next = head;
+        if (head != null) {
+            head.prev = newNode;
+        }
+        return newNode;
+    }
+    public static void printList(Node head) {
+        Node curr = head;
+        while (curr != null) {
+            System.out.print(curr.data);
+            if (curr.next != null) {
+                System.out.print(" <-> ");
+            }
+            curr = curr.next;
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        Node head = new Node(2);
+        head.next = new Node(3);
+        head.next.prev = head;
+        head.next.next = new Node(4);
+        head.next.next.prev = head.next;
+        int data = 1;
+        head = insert(head, data);
+        printList(head);
+    }
+}
