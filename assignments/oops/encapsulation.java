f0a1bdf3f41f8f8dae0938e6807cddd63144325b
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-21 14:39:07 +0530

    Add encapsulation example with Student class

---
+++ assignments/oops/encapsulation.java
@@ -0,0 +1,32 @@
+class Student {
+
+    private String name;
+    private int age;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public static void main(String[] args) {
+
+        Student s = new Student();
+
+        s.setName("Veerendra");
+        s.setAge(21);
+
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+    }
+}
