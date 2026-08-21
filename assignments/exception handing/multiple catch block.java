d2e7fab15f845144b65621c746eab614794498ca
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-21 16:15:07 +0530

    Create multiple catch block.java

---
+++ assignments/exception handing/multiple catch block.java
@@ -0,0 +1,14 @@
+public class MultipleCatchExample {
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length()); // NullPointerException
+        } catch (ArithmeticException e) {
+            System.out.println("Arithmetic Exception occurred!");
+        } catch (NullPointerException e) {
+            System.out.println("Null Pointer Exception occurred!");
+        } catch (Exception e) {
+            System.out.println("General Exception: " + e);
+        }
+    }
+}
