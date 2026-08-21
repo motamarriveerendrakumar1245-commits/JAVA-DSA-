6471c66a2b7ae9011b724b6d3f7b3d4b8ad48467
motamarriveerendrakumar1245-commits <motamarriveerendrakumar1245@gmail.com>
2026-08-21 14:40:59 +0530

    Add multilevel inheritance example with Animal, Dog, and Puppy

---
+++ assignments/oops/inheritance/multilevel inheritance.java
@@ -0,0 +1,29 @@
+class Animal {
+
+    void eat() {
+        System.out.println("Eating");
+    }
+}
+
+class Dog extends Animal {
+
+    void bark() {
+        System.out.println("Barking");
+    }
+}
+
+class Puppy extends Dog {
+
+    void play() {
+        System.out.println("Playing");
+    }
+
+    public static void main(String[] args) {
+
+        Puppy p = new Puppy();
+
+        p.eat();
+        p.bark();
+        p.play();
+    }
+}
